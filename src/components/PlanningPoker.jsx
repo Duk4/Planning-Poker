b11021758652e091a2d3532a3c30ec173804f4c2
Dule <crashendodt@gmail.com>
2019-09-19 20:00:33 +0200

    Setup first component

---
+++ src/components/PlanningPoker.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PlanningPoker extends React.Component {
+    render() {
+        return (
+            <div>Hello world!</div>
+        );
+    }
+}
+
+export default PlanningPoker;
