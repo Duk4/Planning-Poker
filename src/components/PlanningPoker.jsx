51751a49645563ba6c50ef46dc41b86aaeac8969
Dule <crashendodt@gmail.com>
2019-09-20 19:11:44 +0200

    tasks

--- src/components/PlanningPoker.jsx
+++ src/components/PlanningPoker.jsx
@@ -9,3 +9,16 @@ class PlanningPoker extends React.Component {
 }
 
 export default PlanningPoker;
+
+{/*
+Make an task input
+Show all current input
+Submit button...
+
+Make a point system
+Load each task, so they can - ask about repeating task voting
+Collect results and show them
+
+Save the results in localStorage
+Be able to make new votes
+*/}
