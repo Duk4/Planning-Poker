ebc75388e537992a163358a5c0caf921a9bafedd
Dule <crashendodt@gmail.com>
2019-09-23 14:27:06 +0200

    add task component started

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 class AddTask extends React.Component {
     render() {
+        this.state = {
+
+        }
+
         return (
-            <div>Hello World!</div>
+            <div className="add-task">
+                <h1>Add Task</h1>
+                <form>
+                    <div className="input-field">
+                        <input type="text" id="text" onChange="{}" />
+                        <button className="add-btn" onClick="{}">Add</button>
+                    </div>
+                    <div className="added-tasks"></div>
+                    <button className="submit-btn">Submit</button>
+                </form>
+            </div>
         );
     };
 };
