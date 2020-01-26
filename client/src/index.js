9c28f531ba0c8714170905cb060d38c5733d759e
Dule <crashendodt@gmail.com>
2020-01-26 22:03:36 +0100

    removed client folder due to launching new project, leaving only as API

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/style.scss';
-import PlanningPoker from './components/PlanningPoker';
-
-ReactDOM.render(<PlanningPoker />, document.getElementById('root'));
