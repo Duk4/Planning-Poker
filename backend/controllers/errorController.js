f76796b56378dd6b6e566c0713485cda9ce47cc6
Dule <crashendodt@gmail.com>
2020-01-31 16:35:23 +0100

    Show different errors depending on environment

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,10 +1,46 @@
-module.exports = (err, req, res, next) => {
-    console.log(err.stack);
+const AppError = require('../utils/appError');
 
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+const handleSequelizeValidationError = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}.`
+    return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
-        message: err.message
-    })
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        console.error("Error: ", err);
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!'
+        });
+    }
+};
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err };
+
+        if (error.name === "SequelizeValidationError") error = handleSequelizeValidationError(error);
+
+        sendErrorProd(error, res);
+    };
 };
